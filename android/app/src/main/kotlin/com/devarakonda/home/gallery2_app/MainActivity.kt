4e1103eb222918f9c9dd464b32774b4f39787fc7
kirandevarakonda <devarakondasriramkiran@gmail.com>
2020-10-24 01:56:19 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/devarakonda/home/gallery2_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.devarakonda.home.gallery2_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splash.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.image_search_sharp),backgroundColor: Colors.lightGreen[900],),
+      appBar: AppBar(
+        backgroundColor: Colors.lightGreen[900],
+        title: Text('Gallery'),
+        centerTitle: true,
+        leading: Icon(Icons.menu_rounded),
+        actions: [
+          Icon(Icons.account_circle),
+          SizedBox(width: 20,)
+        ],
+      ),
+      body: SingleChildScrollView(
+        child:
+        Column(
+          children: [
+            Row(
+              children: [
+                Image(image: AssetImage('assets/images/1.jpeg'),width: 90,height: 150,),
+                Image(image: AssetImage('assets/images/2.jpeg'),width: 90,height: 150,),
+                Image(image: AssetImage('assets/images/4.jpeg'),width: 100,height: 150,),
+                Image(image: AssetImage('assets/images/5.jpeg'),width: 110,height: 150,),
+              ],
+            ),
+            Row(
+              children: [
+                Image(image: AssetImage('assets/images/6.jpeg'),width: 90,height: 150,),
+                Image(image: AssetImage('assets/images/7.jpeg'),width: 90,height: 150,),
+                Image(image: AssetImage('assets/images/8.jpeg'),width: 90,height: 150,),
+                Image(image: AssetImage('assets/images/9.jpeg'),width: 120,height: 150,),
+              ],
+            ),
+            Row(
+              children: [
+                Image(image: AssetImage('assets/images/10.jpeg'),width: 100,height: 150,),
+                Image(image: AssetImage('assets/images/11.jpeg'),width: 100,height: 150,),
+                Image(image: AssetImage('assets/images/12.jpeg'),width: 100,height: 150,),
+                Image(image: AssetImage('assets/images/13.jpeg'),width: 90,height: 150,),
+              ],
+            ),
+            Row(
+              children: [
+                Image(image: AssetImage('assets/images/14.jpeg'),width: 100,height: 150,),
+                Image(image: AssetImage('assets/images/15.jpeg'),width: 100,height: 150,),
+                Image(image: AssetImage('assets/images/16.jpeg'),width: 100,height: 150,),
+                Image(image: AssetImage('assets/images/17.jpeg'),width: 90,height: 150,),
+              ],
+            ),
+            Row(
+              children: [
+                Image(image: AssetImage('assets/images/18.jpeg'),width: 100,height: 150,),
+                Image(image: AssetImage('assets/images/19.jpeg'),width: 100,height: 150,),
+                Image(image: AssetImage('assets/images/20.jpeg'),width: 100,height: 150,),
+                Image(image: AssetImage('assets/images/21.jpeg'),width: 90,height: 150,),
+              ],
+            ),
+            Row(
+              children: [
+                Image(image: AssetImage('assets/images/22.jpeg'),width: 90,height: 150,),
+                Image(image: AssetImage('assets/images/23.jpeg'),width: 90,height: 150,),
+                Image(image: AssetImage('assets/images/24.jpeg'),width: 90,height: 150,),
+                Image(image: AssetImage('assets/images/25.jpeg'),width: 90,height: 150,),
+              ],
+            ),
+            Row(
+              children: [
+                Image(image: AssetImage('assets/images/1.jpeg'),width: 100,height: 150,),
+                Image(image: AssetImage('assets/images/2.jpeg'),width: 100,height: 150,),
+                Image(image: AssetImage('assets/images/4.jpeg'),width: 100,height: 150,),
+                Image(image: AssetImage('assets/images/5.jpeg'),width: 90,height: 150,),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
